07eae88dddaaf6acf2096bb49c4d741dd8265316
Alexis <alexis_polak@hotmail.com>
2022-08-28 17:54:41 -0300

    created new sidebarcomponent

--- src/Poser.js
+++ src/Poser.js
@@ -8,6 +8,7 @@ import { STLExporter } from './STLExporter';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { DirectionalLight } from 'three';
 import './Poser.css';
+import Sidebar from './Sidebar';
 
 const Poser = () => {
 
@@ -384,38 +385,18 @@ const Poser = () => {
 
     return(
         <div className='app'>
-            <div className='toolbar'>
-                <div className='model__bar'>
-                    <p className='menu__title'> MODEL </p>
-                    <input className='model__load__button' type="file" accept=".fbx" onChange={onModelLoad} />
-                    <div className='model__scale__menu'>
-                        <input className='model__scale__input' ref={scaleRef} type="number" />
-                        <button className='model__scale__update__button' onClick={onUpdateScale}>
-                            <img src='https://img.icons8.com/fluency-systems-filled/452/stretch-diagonally.png'/>
-                        </button>
-                    </div>
-                </div>
-                <div className='animation__bar'>
-                    <p className='menu__title'> ANIMATIONS </p>
-                    <input className='animation__load__button' type="file" accept=".fbx" onChange={onAnimationLoad} multiple/>
-                    <div className='animation__buttons'>
-                        {getAnimationButtons()}
-                    </div> 
-                </div>
-                <div className='animation__inspector'>
-                    <p className='menu__title'> FRAMES </p>
-                    <div className='animation__frames'>
-                        <button className='animation__toggle__button' onClick={onPauseContinue}>
-                            <img src='https://img.icons8.com/ios-glyphs/452/pause--v1.png'/>
-                        </button>
-                        <p className='animation__frames__data'> {animationFrame}/{maxAnimationFrame} </p>
-                    </div>
-                    <input className='animation__frames__slider' type="range" min="0" max={maxAnimationFrame} value={animationFrame} onChange={onAnimationFrameChange} step="1"/>  
-                </div>
-                <div className='export__bar'>     
-                    <button  className='export__button' onClick={bake}> Bake mesh </button>
-                </div>
-            </div>
+            <Sidebar 
+                onModelLoad={onModelLoad}
+                scaleRef={scaleRef}
+                onUpdateScale={onUpdateScale}
+                onAnimationLoad={onAnimationLoad}
+                onPauseContinue={onPauseContinue}
+                animationFrame={animationFrame}
+                maxAnimationFrame={maxAnimationFrame}
+                bake={bake}
+                getAnimationButtons={getAnimationButtons}
+                onAnimationFrameChange={onAnimationFrameChange}
+            />
             <div className='renderer' ref={container}></div>
         </div>
     );
