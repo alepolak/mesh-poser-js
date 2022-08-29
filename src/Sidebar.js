82f2b0777757651510b3d7fbc4d190b9c3f53d4b
Alexis <alexis_polak@hotmail.com>
2022-08-28 22:04:40 -0300

    Added conditional render to the side menu based on the model and animations

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,21 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Sidebar.css';
 import icons from './Icons';
 
 const Sidebar = (props) => {
 
-    return(
-        <div className='sidebar'>
-                <div className='model__bar'>
-                    <p className='menu__title'> MODEL </p>
-                    <input className='model__load__button' type="file" accept=".fbx" onChange={props.onModelLoad} />
-                    <div className='model__scale__menu'>
-                        <input className='model__scale__input' ref={props.scaleRef} type="number" />
-                        <button className='model__scale__update__button' onClick={props.onUpdateScale}>
-                            <img src={icons.scale} alt='Scale model'/>
-                        </button>
-                    </div>
-                </div>
+    const drawAnimationMenu = () => {
+        if(props.modelReady) {
+            return (
                 <div className='animation__bar'>
                     <p className='menu__title'> ANIMATIONS </p>
                     <input className='animation__load__button' type="file" accept=".fbx" onChange={props.onAnimationLoad} multiple/>
@@ -23,6 +14,13 @@ const Sidebar = (props) => {
                         {props.getAnimationButtons()}
                     </div> 
                 </div>
+            );
+        }
+    };
+
+    const drawAnimationPlayerMenu = () => {
+        if(props.hasAnimations) {
+            return (
                 <div className='animation__inspector'>
                     <p className='menu__title'> FRAMES </p>
                     <div className='animation__frames'>
@@ -33,10 +31,36 @@ const Sidebar = (props) => {
                     </div>
                     <input className='animation__frames__slider' type="range" min="0" max={props.maxAnimationFrame} value={props.animationFrame} onChange={props.onAnimationFrameChange} step="1"/>  
                 </div>
+            );
+        }
+    };
+
+    const drawExportMenu = () => {
+        if(props.modelReady) {
+            return (
                 <div className='export__bar'>     
                     <button  className='export__button' onClick={props.bake}> Bake mesh </button>
                 </div>
+            );
+        }
+    };
+
+    return(
+        <div className='sidebar'>
+            <div className='model__bar'>
+                <p className='menu__title'> MODEL </p>
+                <input className='model__load__button' type="file" accept=".fbx" onChange={props.onModelLoad} />
+                <div className='model__scale__menu'>
+                    <input className='model__scale__input' ref={props.scaleRef} type="number" />
+                    <button className='model__scale__update__button' onClick={props.onUpdateScale}>
+                        <img src={icons.scale} alt='Scale model'/>
+                    </button>
+                </div>
             </div>
+            {drawAnimationMenu()}
+            {drawAnimationPlayerMenu()}
+            {drawExportMenu()}
+        </div>
     );
 };
 
