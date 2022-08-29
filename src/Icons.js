522de5c992872fff2748f9c1554e667928dd1354
Alexis <alexis_polak@hotmail.com>
2022-08-28 21:45:39 -0300

    Changed icons to const

--- src/Icons.js
+++ src/Icons.js
@@ -1,11 +1,11 @@
-let play = 'https://img.icons8.com/ios-glyphs/452/play--v1.png';
-let repeat = 'https://img.icons8.com/metro/452/repeat.png';
-let model = 'https://img.icons8.com/external-good-lines-kalash/452/external-human-human-body-anatomy-good-lines-kalash-2.png';
-let animations = 'https://img.icons8.com/ios-filled/452/animation-rig.png';
-let scale = 'https://img.icons8.com/fluency-systems-filled/452/stretch-diagonally.png';
-let pause = 'https://img.icons8.com/ios-glyphs/452/pause--v1.png';
+const play = 'https://img.icons8.com/ios-glyphs/452/play--v1.png';
+const repeat = 'https://img.icons8.com/metro/452/repeat.png';
+const model = 'https://img.icons8.com/external-good-lines-kalash/452/external-human-human-body-anatomy-good-lines-kalash-2.png';
+const animations = 'https://img.icons8.com/ios-filled/452/animation-rig.png';
+const scale = 'https://img.icons8.com/fluency-systems-filled/452/stretch-diagonally.png';
+const pause = 'https://img.icons8.com/ios-glyphs/452/pause--v1.png';
 
-let icons = {
+const icons = {
     play, 
     repeat, 
     model, 
