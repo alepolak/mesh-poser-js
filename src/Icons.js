e65c6e3846c2f418b3d0fe9faecc5babf232aed2
Alexis <alexis_polak@hotmail.com>
2022-09-01 11:37:12 -0300

    Added new icons

--- src/Icons.js
+++ src/Icons.js
@@ -1,7 +1,8 @@
 const play = 'https://img.icons8.com/ios-glyphs/452/play--v1.png';
 const repeat = 'https://img.icons8.com/metro/452/repeat.png';
-const model = 'https://img.icons8.com/external-good-lines-kalash/452/external-human-human-body-anatomy-good-lines-kalash-2.png';
-const animations = 'https://img.icons8.com/ios-filled/452/animation-rig.png';
+const model = 'https://img.icons8.com/ios-filled/452/t-pose.png';
+const frames = 'https://img.icons8.com/ios-filled/452/animation-rig.png';
+const animations = 'https://img.icons8.com/ios-filled/344/running.png';
 const scale = 'https://img.icons8.com/fluency-systems-filled/452/stretch-diagonally.png';
 const pause = 'https://img.icons8.com/ios-glyphs/452/pause--v1.png';
 
@@ -10,6 +11,7 @@ const icons = {
     repeat, 
     model, 
     animations,
+    frames,
     scale,
     pause,
 };
