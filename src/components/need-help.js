dc36070cb8145e7e210605b52d912ac69c659df8
Alexis Polak <alexispolak@gmail.com>
2022-12-26 18:48:34 -0300

    Update the help section.

--- src/components/need-help.js
+++ src/components/need-help.js
@@ -3,7 +3,7 @@ import './need-help.css';
 
 const NeedHelp = () => {
     return (
-        <a href='https://www.loom.com/share/3b592a51f62741c9a2c7255c0805cb0b' target={"_blank"}>Need help?</a>
+        <a href='https://github.com/alepolak/mesh-poser-js#examples' target={"_blank"} rel="noopener">Need help?</a>
     );
 };
 
