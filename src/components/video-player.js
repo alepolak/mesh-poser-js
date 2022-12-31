e6ed1f24ffd5c8f9f3d2e9e02847de30b04657be
Alexis Polak <alexispolak@gmail.com>
2022-12-31 17:15:27 -0300

    Added format depending on user agent

--- src/components/video-player.js
+++ src/components/video-player.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './video-player.css';
+import { getFormatByBrowser} from '../video-tutorial-resources';
 
 const VideoPlayer = (props) => {
     return(
         <video className="video__player" key={props.key} autoPlay loop muted>
-            <source src={props.url} type="video/webm" className='video__source'/>
+            <source src={props.url} type={`video/${getFormatByBrowser()}`} className='video__source'/>
         </video>
     );
 };
